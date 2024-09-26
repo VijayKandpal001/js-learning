3c3ea213535214072d79f25f1d89752242ad4241
Vijay Kandpal <151955107+VijayKandpal001@users.noreply.github.com>
2024-09-26 10:07:28 +0000

    some files updated.

---
+++ 01_basics/1.2_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all javascript code as new version
+
+// alert(2+3) //will work on browser not using nodejs
+
+console.log(23+2); console.log("JAI SHREE RAM") // this will work but not considered as good practice.
+
+let name="Ranchordas"
+let age=23
+let isEnginner=true
+
+/*
+other types of datatypes:
+number 
+bigint
+string= => ""
+boolean => true/false
+null => standalone value  
+undefined
+symbol => used for uniqueness 
+
+*/
+
+console.log(typeof name);
+console.log(typeof isEnginner);
+console.log(typeof null); // type is object
+console.log(typeof undefined); // type is undefined
