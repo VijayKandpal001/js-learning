0c2c04eaebebe859b9a7fbc92fea545293f5235f
Vijay Kandpal <151955107+VijayKandpal001@users.noreply.github.com>
2024-09-27 05:49:45 +0000

    more about strings covered.

--- 01_basics/1.3_datatype_conversion.js
+++ 01_basics/1.3_datatype_conversion.js
@@ -1,32 +1,56 @@
-let num="12"
+// let num="12"
 
-console.log(num);
-console.log(typeof num);
+// console.log(num);
+// console.log(typeof num);
 
-let conversion=Number(num)
-console.log(conversion);
-console.log(typeof conversion);
+// let conversion=Number(num)
+// console.log(conversion);
+// console.log(typeof conversion);
 
 
-let  num1="123A"
-console.log( num1);
-console.log( typeof num1);
-console.log( typeof(num1));
+// let  num1="123A"
+// console.log( num1);
+// console.log( typeof num1);
+// console.log( typeof(num1));
 
-let  conversion2=Number(num1)
-console.log(conversion2); // returns NaN (i.e Not a Number) and NaN is having number datatype.
-console.log(typeof conversion2);
+// let  conversion2=Number(num1)
+// console.log(conversion2); // returns NaN (i.e Not a Number) and NaN is having number datatype.
+// console.log(typeof conversion2);
 
-let a=1,b=0
-console.table([Boolean(a),Boolean(b)]);
-/*
-    1 -> true
-    0 -> false
-    "" -> false
-    "xyz" ->true
-*/
+// let a=1,b=0
+// console.table([Boolean(a),Boolean(b)]);
+// /*
+//     1 -> true
+//     0 -> false
+//     "" -> false
+//     "xyz" ->true
+// */
 
-let x=undefined,y=null
-console.table([typeof x,typeof y])
-console.table([Number(x),Number(y)])
+// let x=undefined,y=null
+// console.table([typeof x,typeof y])
+// console.table([Number(x),Number(y)])
 
+
+
+
+
+
+
+
+
+/*               OPERATIONS        */
+let str1 = "JAI"
+let str2 = " SHREE RAM"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2); //return 12
+// console.log(1 + "2"); //return 12
+// console.log("1" + 2 + 3); // return 123
+// console.log(2 + 2 + "3"); //return 43
+
+console.log(true)
+console.log(+true) //return 1
+
+console.log(+"") //return 0
